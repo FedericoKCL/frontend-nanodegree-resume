19447e3d194c649b9b6fb91d0f539a0203dc446a
FedericoKCL <fed.deployment@aol.co.uk>
2016-07-05 08:41:26 +0100

    added two elements on the page with js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,13 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var name = 'Federico';
+var role = 'udacity code reviwer';
+
+
+var formattedName =
+    HTMLheaderName.replace('%data%', bio.name);
+var formattedRole =
+    HTMLheaderRole.replace('%data%', bio.role);
+var formattedWelcome = HTML
+
+
+$('#header').prepend(formattedRole);
+$('#header').prepend(formattedName);

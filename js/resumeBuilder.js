c3ae7b66fe1ef4413c767f4a198eb499ad048f9a
FedericoKCL <fed.deployment@aol.co.uk>
2016-07-06 18:30:40 +0100

    refactored builer.js file'

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,31 +1,56 @@
-var name = 'Federico';
-var role = 'udacity code reviwer';
-
 var bio = {
-    'name' : 'Federico',
-    'role' : 'Udacity Code Reviewer',
-    'contacts' : 'federicocapaldo@yahoo.co.uk',
-    'skills' : [
-    'programming', 'python', 'javascript', 'rails', 'java', 'html', 'css', 'ux'
-    ],
-    'welcomeMessage' : 'Welcome on my Resume!',
-    'bioPic': 'images/fry.jpg'
+  "name" : "Federico",
+  "role" : "Udacity Code Reviewer",
+  "contacts" : "federicocapaldo@yahoo.co.uk",
+  "skills" : [
+  "programming", "python", "javascript", "rails", "java", "html", "css", "ux"
+  ],
+  "welcomeMessage" : "Welcome on my Resume!",
+  "bioPic": "images/fry.jpg"
 };
 
-$('#main').append(bio.name);
 
-work = {
-    'company': 'Secret Web Developer Company',
-    'year': '2015 - 2016',
-    'position': 'Web Developer Intern',
-    'city': 'London, UK'
+var work = {
+  "jobs": [
+    {
+      "employer": "Secret Web Developer Company",
+      "title": "Web Developer Intern",
+      "dates": "2015 - 2016",
+      "description": "started off with little of no knowledge of Web Development and endend up\
+      in partecipating in important projects as a full stack web developer for clients like\
+      Tesco, Fortunm and Mason, Camdenmarket."
+    }
+  ]
 };
+
 
 var education = {
-    'name': 'King\'s College London',
-    'years': '2014 - 2017',
-    'city': 'London, UK'
+  "schools": [{
+    "name": "Kings College",
+    "location": "London",
+    "degree": "BSc",
+    "majors": ["Computer Science"],
+    "dates": "2018"
+  }, {
+    "name": "Williams College",
+    "location": "London",
+    "degree": "HND",
+    "majors": ["Computing & Systems Development"],
+    "dates": 2014
+  }],
+  "onlineCourses": [{
+    "title": "Javascript",
+    "school": "Udacity",
+    "dates": 2014,
+    "url": "http://www.udacity.com/course/ud804"
+  }]
 };
 
-$('#main').append(work['position']);
-$('#main').append(education.name);
+
+var projects = {
+  "projects": [
+    "title": "Chemical Trace",
+    "dates": "2015/2016",
+    "description": "First Website in the world that helps you understand the content of your labels"
+  ]
+}
